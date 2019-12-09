64d2c77d54c7ba720a5e928d65d85506c5a13f45
sedafdemir <57167050+sedafdemir@users.noreply.github.com>
2019-12-09 12:58:08 -0600

    This is first test

---
+++ src/gitTesting/TestClass1.java
@@ -0,0 +1,12 @@
+package gitTesting;
+
+public class TestClass1 {
+    public static void main(String[] args) {
+        String word = "Yes";
+        if(word.equals("yes")){
+            System.out.println("Eat something");
+        }else {
+            System.out.println("Continue to work");
+        }
+    }
+}
